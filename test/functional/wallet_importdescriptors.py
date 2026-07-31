e2cadf821a66b463ceca1a49121fe6b61f9537eb
kingpepe2 <kingpepe2@users.noreply.github.com>
2026-08-01 01:20:11 +0400

    remediation(KP-AUD-005): locate imported descriptor test outpoint

--- test/functional/wallet_importdescriptors.py
+++ test/functional/wallet_importdescriptors.py
@@ -431,9 +431,9 @@ class ImportDescriptorsTest(BitcoinTestFramework):
                      address,
                      solvable=True,
                      ismine=True)
-        txid = w0.sendtoaddress(address, 49.99995540)
+        utxo = self.create_outpoints(w0, outputs=[{address: 49.99995540}])[0]
         self.generatetoaddress(self.nodes[0], 6, w0.getnewaddress())
-        tx = wpriv.createrawtransaction([{"txid": txid, "vout": 0}], {w0.getnewaddress(): 49.999})
+        tx = wpriv.createrawtransaction([utxo], {w0.getnewaddress(): 49.999})
         signed_tx = wpriv.signrawtransactionwithwallet(tx)
         w1.sendrawtransaction(signed_tx['hex'])
 
