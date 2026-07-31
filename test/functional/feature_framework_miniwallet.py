6ae569d2fe3068eed11c736a43c6344a2b41a1ce
kingpepe2 <kingpepe2@users.noreply.github.com>
2026-07-31 20:45:14 +0400

    remediation(KP-AUD-005): scale MiniWallet padding test fees for KingPepe

--- test/functional/feature_framework_miniwallet.py
+++ test/functional/feature_framework_miniwallet.py
@@ -5,6 +5,7 @@
 """Test MiniWallet."""
 import random
 import string
+from decimal import Decimal
 
 from test_framework.blocktools import COINBASE_MATURITY
 from test_framework.test_framework import BitcoinTestFramework
@@ -24,12 +25,13 @@ class FeatureFrameworkMiniWalletTest(BitcoinTestFramework):
     def test_tx_padding(self):
         """Verify that MiniWallet's transaction padding (`target_vsize` parameter)
            works accurately with all modes."""
+        fee_rate = Decimal("0.000001")
         for mode_name, wallet in self.wallets:
             self.log.info(f"Test tx padding with MiniWallet mode {mode_name}...")
             utxo = wallet.get_utxo(mark_as_spent=False)
             for target_vsize in [250, 500, 1250, 2500, 5000, 12500, 25000, 50000, 1000000,
                                  248, 501, 1085, 3343, 5805, 12289, 25509, 55855,  999998]:
-                tx = wallet.create_self_transfer(utxo_to_spend=utxo, target_vsize=target_vsize)
+                tx = wallet.create_self_transfer(utxo_to_spend=utxo, target_vsize=target_vsize, fee_rate=fee_rate)
                 assert_equal(tx['tx'].get_vsize(), target_vsize)
                 child_tx = wallet.create_self_transfer_multi(utxos_to_spend=[tx["new_utxo"]], target_vsize=target_vsize)
                 assert_equal(child_tx['tx'].get_vsize(), target_vsize)
